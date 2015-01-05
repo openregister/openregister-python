f64abaf769d90b338be91da3632ddfafd66244c1
psd <psd@whatfettle.com>
2015-01-05 21:20:30 +0000

    Placeholder Thingstance module

--- thingstance/thingstance.py
+++ thingstance/thingstance.py
@@ -2,5 +2,8 @@ class Thingstance(object):
     """An instance of a Thing, with metadata."""
     name = None
     type = "Thing"
-    thing = Thing()
-    tags = []
+    thing = None
+    tags = set()
+
+    def __init__(self, thing):
+        self.thing = thing
