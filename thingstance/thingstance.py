695c7738a57bfb29521c46b47304956b3ef17ccd
psd <psd@whatfettle.com>
2015-01-08 15:12:17 +0000

    Thingstance is just another Thing

--- thingstance/thingstance.py
+++
@@ -1,9 +0,0 @@
-class Thingstance(object):
-    """An instance of a Thing, with metadata."""
-    name = None
-    type = "Thing"
-    thing = None
-    tags = set()
-
-    def __init__(self, thing):
-        self.thing = thing
