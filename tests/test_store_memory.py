3e275d3f2b9bacc6102b426c4805b2a9419fbd9d
psd <psd@whatfettle.com>
2015-01-08 15:38:53 +0000

    Store tests

--- tests/test_store_memory.py
+++ tests/test_store_memory.py
@@ -5,11 +5,25 @@ store = MemoryStore()
 
 
 def test_memory_store():
-    emptyThing = Thing()
-    hash = emptyThing.hash
+    thing = Thing()
+    empty_hash = thing.hash
+    store.put(thing)
 
-    store.put(emptyThing)
+    thing = Thing(text='Foo Value')
+    foo_hash = thing.hash
+    store.put(thing)
 
-    thing = store.get(hash)
+    thing = Thing(text='Bar Value')
+    bar_hash = thing.hash
+    store.put(thing)
 
-    assert thing.hash == hash
+    thing = store.get(empty_hash)
+    assert thing.hash == empty_hash
+
+    thing = store.get(foo_hash)
+    assert thing.hash == foo_hash
+    assert thing.text == 'Foo Value'
+
+    thing = store.get(bar_hash)
+    assert thing.hash == bar_hash
+    assert thing.text == 'Bar Value'
