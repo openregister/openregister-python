a3dc3d3ec4109379937432584f1678eba31352d7
psd <psd@whatfettle.com>
2015-01-08 16:49:33 +0000

    Find latest version by name

--- tests/test_store_memory.py
+++ tests/test_store_memory.py
@@ -27,3 +27,29 @@ def test_memory_store():
     thing = store.get(bar_hash)
     assert thing.hash == bar_hash
     assert thing.text == 'Bar Value'
+
+
+def test_memory_get_latest_by_name():
+    store.put(Thing(name="toves", text='Slithy'))
+    store.put(Thing(name="borogoves", text='Mimsy'))
+
+    thing = store.get_latest(name="toves")
+    assert thing.name == "toves"
+    assert thing.text == "Slithy"
+
+    v1 = store.get_latest(name="borogoves")
+    assert v1.name == "borogoves"
+    assert v1.text == "Mimsy"
+
+    v2 = Thing(name="borogoves", text='All mimsy')
+    store.put(v2)
+
+    thing = store.get_latest(name="borogoves")
+    assert thing.hash == v2.hash
+    assert thing.name == "borogoves"
+    assert thing.text == "All mimsy"
+
+    thing = store.get(v1.hash)
+    assert thing.hash == v1.hash
+    assert thing.name == "borogoves"
+    assert thing.text == "Mimsy"
--- thingstance/store/memory.py
+++ thingstance/store/memory.py
@@ -3,12 +3,21 @@ class MemoryStore(object):
     """Simple in-memory storage for Things."""
 
     things = {}
-
-    def get(self, hash):
-        return self.things[hash]
+    latest = {}
 
     def put(self, thing):
         hash = thing.hash
         self.things[hash] = thing
+
+        try:
+            self.latest[thing.name] = thing
+        except AttributeError:
+            pass
+
+    def get(self, hash):
+        return self.things[hash]
+
+    def get_latest(self, name=None):
+        return self.latest[name]
 
 store = MemoryStore()
