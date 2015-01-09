4e7bd892750021765256687bff73e2427ff5455d
psd <psd@whatfettle.com>
2015-01-09 16:38:15 +0000

    Removed stream loader for now

--- thingstance/representation/yaml.py
+++ thingstance/representation/yaml.py
@@ -6,20 +6,5 @@ def dump(self):
     return yaml.dump(self.__dict__, default_flow_style=False)
 
 
-def stream_load(stream):
-    block = stream.readline()
-    cont = 1
-    while cont:
-        line = stream.readline()
-        if len(line) == 0:
-            cont = 0
-        else:
-            if line.startswith(' '):
-                block = block + line
-            else:
-                yield block
-                block = line
-
-
 def register():
     Thing.yaml = property(dump)
