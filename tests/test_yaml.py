08d48b1ddd8299b5b15f93cb3f296906fa0c558b
psd <psd@whatfettle.com>
2015-01-09 19:44:21 +0000

    Load from yaml

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -31,3 +31,35 @@ def test_set_of_tags_as_yaml():
                     '  c: null\n'
                     '  z: null\n'
                     'name: foo\n')
+
+
+def test_empty_thing_from_yaml():
+    thing = Thing()
+    thing.yaml = ('{}')
+    assert thing.yaml == ('{}\n')
+
+
+def test_postaladdress_from_yaml():
+    thing = Thing()
+    thing.yaml = ('addressCountry: GB\n'
+                  'addressLocality: Holborn\n'
+                  'addressRegion: London\n'
+                  'postcode: WC2B 6NH\n'
+                  'streetAddress: Aviation House, 125 Kingsway\n')
+
+    assert thing.streetAddress == "Aviation House, 125 Kingsway"
+    assert thing.addressLocality == "Holborn"
+    assert thing.addressRegion == "London"
+    assert thing.postcode == "WC2B 6NH"
+    assert thing.addressCountry == "GB"
+
+
+def test_set_of_tags_from_yaml():
+    thing = Thing()
+    thing.yaml = ('fields: !!set\n'
+                  '  b: null\n'
+                  '  c: null\n'
+                  '  z: null\n'
+                  'name: foo\n')
+    assert thing.name == "foo"
+    assert thing.fields == {'b', 'c', 'z'}
--- thingstance/representation/yaml.py
+++ thingstance/representation/yaml.py
@@ -2,9 +2,15 @@ from ..thing import Thing
 import yaml
 
 
+def load(self, text):
+    """Thing from YAML representation."""
+    self.__dict__ = yaml.load(text)
+
+
 def dump(self):
+    """Thing in YAML representation."""
     return yaml.dump(self.__dict__, default_flow_style=False)
 
 
 def register():
-    Thing.yaml = property(dump)
+    Thing.yaml = property(dump, load)
