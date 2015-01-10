5983738e8df368b1ee7e6e5f697e28789b2d0f0e
psd <psd@whatfettle.com>
2015-01-10 11:48:46 +0000

    Refactored hash into function

--- thingstance/thing.py
+++ thingstance/thing.py
@@ -1,6 +1,12 @@
 from hashlib import sha1
 
 
+def hash(data):
+    head = str("blob " + str(len(data)) + "\0").encode("utf-8")
+    digest = sha1(head + data)
+    return digest.hexdigest()
+
+
 class Thing(object):
     """A Thing, a content addressable set of attributes."""
     def __init__(self, **kwds):
@@ -9,6 +15,4 @@ class Thing(object):
     @property
     def hash(self):
         """A digest of the Thing; the git hash-object value of the json."""
-        data = self.json.encode("utf-8")
-        head = str("blob " + str(len(data)) + "\0").encode("utf-8")
-        return sha1(head + data).hexdigest()
+        return hash(self.json.encode("utf-8"))
