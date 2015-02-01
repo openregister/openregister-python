8b49f0ede7714a421c9f2e8c4fbbbea3e1479459
psd <psd@whatfettle.com>
2015-02-01 20:48:22 +0100

    TXT Writer

---
+++ tests/test_writer_txt.py
@@ -0,0 +1,42 @@
+import io
+from thingstance import Thing
+from thingstance.representations.txt import Writer
+
+
+def test_writer_zero_things():
+    out = io.StringIO()
+    writer = Writer(out)
+    writer.close()
+
+    string = out.getvalue()
+    assert string == ""
+
+
+def test_writer_one_thing():
+    out = io.StringIO()
+    writer = Writer(out)
+    thing = Thing(name="one")
+    writer.write(thing)
+    writer.close()
+
+    string = out.getvalue()
+    assert string == '- name: one\n'
+
+
+def test_writer_many_things():
+    out = io.StringIO()
+    writer = Writer(out)
+    for name in ['one', 'two', 'three']:
+        thing = Thing(name=name, text="hello world")
+        writer.write(thing)
+    writer.close()
+
+    string = out.getvalue()
+    assert string == ('- name: one\n'
+                      '  text: hello world\n'
+                      '\n'
+                      '- name: two\n'
+                      '  text: hello world\n'
+                      '\n'
+                      '- name: three\n'
+                      '  text: hello world\n')
