c41421d93f6ab0a44b63f4ad0eb98c2f5fb8b1a3
psd <psd@whatfettle.com>
2015-11-17 09:01:32 +0000

    Test reader interface

---
+++ tests/test_reader.py
@@ -0,0 +1,16 @@
+import pytest
+import io
+from openregister.reader import Reader
+
+
+def test_writer_interface():
+    stream = io.StringIO()
+
+    reader = Reader(stream)
+
+    assert reader.stream == stream
+
+    with pytest.raises(NotImplementedError):
+        item = reader.read()
+
+    reader.close()
